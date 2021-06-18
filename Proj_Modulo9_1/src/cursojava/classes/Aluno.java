5c6b1218758da28d55cb7c473218842ee3cd928b
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2021-06-17 22:05:19 -0300

    acrescentando metodos setters e getters a classe aluno

--- Proj_Modulo9_1/src/cursojava/classes/Aluno.java
+++ Proj_Modulo9_1/src/cursojava/classes/Aluno.java
@@ -2,17 +2,57 @@ package cursojava.classes;
 
 public class Aluno {
 	//Atributos do Aluno, ou seja , as caracteristicas do aluno no mundo real
-		public String nome; //para acesso direto por outras classes
-		public int idade;
+		//public String nome; //para acesso direto por outras classes
+		private String nome;//padrão para proteger a variavel de acesso direto
+		private int idade;
 		private String dataNascimento; //atributo protegido, acessado apenas por metodos e pela propria classe
-		String registroGeral;
-		String numeroCpf;
-		String nomeMae;
-		String nomePai;
-		String dataMatricula;
-		String nomeEscola;
-		String serieMatriculada;
+		private String registroGeral;
+		private String numeroCpf;
+		private String nomeMae;
+		private String nomePai;
+		private String dataMatricula;
+		private String nomeEscola;
+		private String serieMatriculada;
 		
+		//atributos para calculo de média
+		private double nota1;
+		private double nota2;
+		private double nota3;
+		private double nota4;
+		
+		//gets e sets para media		
+		public double getNota1() {
+			return nota1;
+		}
+
+		public void setNota1(double nota1) {
+			this.nota1 = nota1;
+		}
+
+		public double getNota2() {
+			return nota2;
+		}
+
+		public void setNota2(double nota2) {
+			this.nota2 = nota2;
+		}
+
+		public double getNota3() {
+			return nota3;
+		}
+
+		public void setNota3(double nota3) {
+			this.nota3 = nota3;
+		}
+
+		public double getNota4() {
+			return nota4;
+		}
+
+		public void setNota4(double nota4) {
+			this.nota4 = nota4;
+		}
+
 		//construtores Aluno aluno4 = new Aluno();
 		/*é o mesmo que estar referenciando um construtor no java*/
 		public Aluno() {//construtor padrão do java - cria os dados na memoria		
@@ -27,5 +67,106 @@ public class Aluno {
 			nome = nomePadrao;
 			idade = idadePadrao;
 		}
+		
+		//GET e SET		
+		/*Métodos SETTERS e GETTERS do objeto, onde o SET é para adicionar ou receber dados 
+		 * para os atributos e GET é para resgatar ou obter o valor/dado do atributo. Todos os 
+		 * objetos em java tem.
+		 * public porque tem que ser acessado de qualquer lugar
+		 * void porque ele não vai retornar só vai receber valor
+		 * this (este) se refere a classe, nome ao atributo, e string nome a variavel que 
+		 * receberá  o dado*/
+		
+		//metodo para receber dado
+		public void setNome (String nome) {
+			this.nome = nome;
+		}
+		
+		/*O GET é similar ao set pois é publico para ser acessado, deve conter o tipo de dado
+		 * retornado (String- texto-, int- intreiro-, etc) a palavra GET seguido da variavel
+		 * com inicial maiuscula, o que se deseja retornar com a palavra 'return' seguida da
+		 * variavel */
+		//metodo que retorna (return) o dado
+		public String getNome() {
+			return nome;
+		}
 
+		public int getIdade() {
+			return idade;
+		}
+
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+
+		public String getDataNascimento() {
+			return dataNascimento;
+		}
+
+		public void setDataNascimento(String dataNascimento) {
+			this.dataNascimento = dataNascimento;
+		}
+
+		public String getRegistroGeral() {
+			return registroGeral;
+		}
+
+		public void setRegistroGeral(String registroGeral) {
+			this.registroGeral = registroGeral;
+		}
+
+		public String getNumeroCpf() {
+			return numeroCpf;
+		}
+
+		public void setNumeroCpf(String numeroCpf) {
+			this.numeroCpf = numeroCpf;
+		}
+
+		public String getNomeMae() {
+			return nomeMae;
+		}
+
+		public void setNomeMae(String nomeMae) {
+			this.nomeMae = nomeMae;
+		}
+
+		public String getNomePai() {
+			return nomePai;
+		}
+
+		public void setNomePai(String nomePai) {
+			this.nomePai = nomePai;
+		}
+
+		public String getDataMatricula() {
+			return dataMatricula;
+		}
+
+		public void setDataMatricula(String dataMatricula) {
+			this.dataMatricula = dataMatricula;
+		}
+
+		public String getNomeEscola() {
+			return nomeEscola;
+		}
+
+		public void setNomeEscola(String nomeEscola) {
+			this.nomeEscola = nomeEscola;
+		}
+
+		public String getSerieMatriculada() {
+			return serieMatriculada;
+		}
+
+		public void setSerieMatriculada(String serieMatriculada) {
+			this.serieMatriculada = serieMatriculada;
+		}
+		
+		//Método que retorna a media do aluno
+		public double getMediaNota() {
+			return (nota1 + nota2 + nota3 + nota4)/ 4;
+		}
+		
+		
 }
--- Proj_Modulo9_1/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Proj_Modulo9_1/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -21,10 +21,55 @@ public class PrimeiraClasseJava {
 		
 		//criando um objeto real na memoria
 		Aluno aluno1 = new Aluno();//aluno joão.
-		aluno1.nome = "João";
-		aluno1.idade = 43;
+		/*aluno1.nome = "João"; para atributo public seria assim. Errado
+		aluno1.idade = 43;*/
+		
+		//correto. Com metodo set e get
+		//dados do alluno joão
+		aluno1.setNome("João da silva");
+		aluno1.setIdade(20);
+		aluno1.setDataNascimento("14/05/2001"); //atributo protegido, acessado apenas por 
+		aluno1.setRegistroGeral("8.888.445");//metodos e pela propria classe
+		aluno1.setNumeroCpf("055.999.666-00");
+		aluno1.setNomeMae("Carla");
+		aluno1.setNomePai("Marcus Aurelio");
+		aluno1.setDataMatricula("30/02/2019");
+		aluno1.setNomeEscola("FGv");
+		aluno1.setSerieMatriculada("1ª");
+		//notas recebendo valores
+		aluno1.setNota1(80.5);
+		aluno1.setNota2(50.5);
+		aluno1.setNota3(65.4);
+		aluno1.setNota4(73.5);		
+		
+		//mostrando os dados de João
+		System.out.println("O nome é: "+ aluno1.getNome());//o get não se usa em qualquer lugar
+		System.out.println("A idade é: "+ aluno1.getIdade());// apena para recuperar o dado
+		System.out.println("A data de nascimento é: "+ aluno1.getDataNascimento());
+		System.out.println("O RG é: "+ aluno1.getRegistroGeral());
+		System.out.println("O CPF é: "+ aluno1.getNumeroCpf());
+		System.out.println("O nome da mãe é: "+ aluno1.getNomeMae());
+		System.out.println("O nome do pai é: "+ aluno1.getNomePai());
+		System.out.println("A matricula é: "+ aluno1.getDataMatricula());
+		System.out.println("O nome da escola é: "+ aluno1.getNomeEscola());
+		System.out.println("A serie matriculada é: "+ aluno1.getSerieMatriculada());
+		//mostrando media com 2 casas decimais no printf onde % é um coringa onde entra a variavel
+		
+		System.out.printf("A média das notas são: %.2f \n", aluno1.getMediaNota());
+		
+		System.out.println("======================== FIM aluno "+ aluno1.getNome() +" ====================================");
 		
 		Aluno aluno2 = new Aluno();//aluno pedro
+		aluno2.setNome("Pedro Rosa");
+		aluno2.setIdade(20);
+		aluno2.setDataNascimento("18/10/2001");
+		
+		System.out.println("O nome é: "+ aluno2.getNome());//o get não se usa em qualquer lugar
+		System.out.println("A idade é: "+ aluno2.getIdade());// apena para recuperar o dado
+		System.out.println("A data de nascimento é: "+ aluno2.getDataNascimento());
+		System.out.println("idem ao aluno 1");
+		
+		System.out.println("======================== FIM aluno "+ aluno2.getNome() +" ====================================");
 		Aluno aluno3 = new Aluno();//aluno alex
 		Aluno aluno4 = new Aluno("Maria"); //criando objeto com parametro
 		Aluno aluno5 = new Aluno("José", 50);
