3fde1edf9523766c7b31f2726602d941e7830510
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2021-06-20 19:09:59 -0300

    idem comit 20/06/21

--- Proj_Modulo9_1/src/cursojava/constantes/StatusAluno.java
+++ Proj_Modulo9_1/src/cursojava/constantes/StatusAluno.java
@@ -4,6 +4,5 @@ public class StatusAluno {
 	/*Classe e atributos estatícos, ou conhecidas como constante, a variavel é maiuscula*/
 	public static String APROVADO = "Aprovado";
 	public static String REPROVADO = "Reprovado";
-	public static String RECUPERACAO = "Recuperacao";
-	
+	public static String RECUPERACAO = "Recuperacao";	
 }
